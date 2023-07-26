fd97506afcdcac77a93850ac7ae3cd17bc2a1c98
luowei li <liluowei@tuyoogame.com>
2023-07-26 10:27:24 +0800

    test(flink-learning):test

--- flink-learning-basic/flink-learning-data-sinks/src/main/java/com/zhisheng/data/sinks/Main2.java
+++ flink-learning-basic/flink-learning-data-sinks/src/main/java/com/zhisheng/data/sinks/Main2.java
@@ -9,7 +9,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  * https://t.zsxq.com/EIiyjeU
  * Created by zhisheng on 2019-09-26
  * blog：http://www.54tianzhisheng.cn/
- * 微信公众号：zhisheng
+ *
  */
 public class Main2 {
     public static void main(String[] args) throws Exception {
